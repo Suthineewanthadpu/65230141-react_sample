9ef2d2507978fb460edcea68e5100a0821272bb1
Suthinee Wantha <suthineewantha@Suthinees-MacBook-Air.local>
2023-02-18 15:40:11 +0700

    setup gh-pages

--- src/report.jsx
+++ src/report.jsx
@@ -7,6 +7,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Table from "react-bootstrap/Table";
+import Image from 'react-bootstrap/Image'
 import axios from "axios";
 import Common from "./common";
 const BASE_URL = Common.API_URL;
@@ -65,7 +66,9 @@ export default class report extends Component {
           <Container>
             <Navbar.Brand href="#">ค้นหาเลขไปรษณีย์</Navbar.Brand>
             <div style={{color:"#FFFF00", fontSize:"25px"}}> 
-            65230141 สุธินี วรรณทา
+            <Image src="https://scontent.fbkk15-1.fna.fbcdn.net/v/t39.30808-6/247990006_3036881779925186_3231696389517404293_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHErKQtjgXk_BcbOUw7vn1ebydbGiWaGAxvJ1saJZoYDPiUbjcaWsyXMZ4XzQ9sXajrHfTehkhjOKgAK4_i9smy&_nc_ohc=jSLaU9qxW-cAX_slPIH&_nc_ht=scontent.fbkk15-1.fna&oh=00_AfCrdeM6rC1uJaXkicfjiEGLRn2KdGj3wsP1Oxox_b0XaA&oe=63F5CBF1" 
+            roundedCircle thumbnail style={{width:"78px", height : "78px"}}/> 
+{" "}65230141 Suthinee Wantha
             </div>
           </Container>
         </Navbar>
